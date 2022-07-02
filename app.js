ba621ad696de17fe3f523071ad8c8a51f8bdfc6a
Debajyoti Halder <debajyotih@iitbhilai.ac.in>
2022-07-02 16:41:05 +0530

    webtorrent playground

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname + '/public'))
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
