a40396f02ee7572ed3f8c2380fb3265a1a4e8c34
Nandini Khandelwal <nandinikhandelwal20.set@modyuniversity.ac.in>
2022-07-05 18:18:50 +0530

    no changes

--- public/js/client.js
+++ public/js/client.js
@@ -15,7 +15,7 @@ send.addEventListener('click', (e) => {
 
     const files = document.getElementById("file").files;
 
-    client.seed(files, function(torrent) {
+    client.seed(files, function (torrent) {
         console.log('Client is seeding ' + torrent.magnetURI)
         socket.emit('sendMessage', torrent.magnetURI);
     })
@@ -35,11 +35,11 @@ socket.on('sendToClient', (torrentID) => {
 
     client.add(torrentID, (torrent) => {
         console.log(torrent);
-        let check = setInterval(() => {
+        let check = setInterval(() => {           //for checking progress every 1000ms
             document.querySelector('#progress').value = torrent.progress * 100;
             if (torrent.progress == 1) {
                 torrent.files.forEach(file => {
-                    file.getBlobURL(function(err, url) {
+                    file.getBlobURL(function (err, url) {
                         if (err) throw err
                         const a = document.createElement('a')
                         a.download = file.name
@@ -56,46 +56,5 @@ socket.on('sendToClient', (torrentID) => {
 
         }, 1000);
     })
-
-    // client.on('torrent', (torrent) => {
-    //     console.log(torrent);
-    //     // const file1 = torrent.files.find(function(file) {
-    //     //     return file.name.endsWith('.jpg')
-    //     // })
-    //     let check = setInterval(() => {
-    //         document.querySelector('#progress').value = torrent.progress * 100;
-    //         if (torrent.progress == 1) {
-    //             torrent.files.forEach(file => {
-    //                 file.getBlobURL(function(err, url) {
-    //                     if (err) throw err
-    //                     const a = document.createElement('a')
-    //                     a.download = file.name
-    //                     a.href = url
-    //                     a.textContent = 'Download ' + file.name
-    //                     document.body.appendChild(a);
-    //                     a.click();
-    //                     a.remove();
-    //                 })
-    //             });
-    //             clearInterval(check);
-    //         }
-
-    //     }, 1000);
-    //     // file1.appendTo('body')
-    // })
 })
 
-function outputMessage(message) {
-    const div = document.createElement('div');
-    div.classList.add('message');
-    const p = document.createElement('p');
-    p.classList.add('meta');
-    p.innerText = message.username;
-    p.innerHTML += `<span>${message.time}</span>`;
-    div.appendChild(p);
-    const para = document.createElement('p');
-    para.classList.add('text');
-    para.innerText = message.text;
-    div.appendChild(para);
-    document.querySelector('.chat-messages').appendChild(div);
-}
