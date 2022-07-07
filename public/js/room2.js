549a79947b4a76436dc87bfe3b1867ea8870a74f
Debajyoti Halder <debajyotih@iitbhilai.ac.in>
2022-07-08 02:57:23 +0530

    new UI

--- public/js/room2.js
+++ public/js/room2.js
@@ -1,12 +1,70 @@
 function copyToClipboard(element) {
-    var $temp = $("<h2>");
-    $(".roomID").append($temp);
-    $temp.val($(element).text()).select();
-    document.execCommand("copy");
-    $temp.remove();
+    // var $temp = $("");
+    // $(".roomID").append($temp);
+    // $temp.val($(element).text()).select();
+    // document.execCommand("copy");
+    // $temp.remove();
 }
 
-const add = document.querySelector(".add");
-add.addEventListener("click", () => {
+// const add = document.querySelector(".add");
+// add.addEventListener("click", () => {
 
-})
+// })
+
+Highcharts.chart('peers', {
+    chart: {
+        type: 'networkgraph',
+        // color: 'rgba(255, 255, 255, 0)'
+    },
+    title: {
+        text: 'Peer graph',
+        color: '#ccc'
+    },
+    plotOptions: {
+        networkgraph: {
+            layoutAlgorithm: {
+                enableSimulation: true
+            }
+        }
+    },
+
+    series: [{
+        dataLabels: {
+            enabled: true,
+            // linkTextPath: {
+            //     attributes: {
+            //         dy: 12
+            //     }
+            // },
+            linkFormat: '',
+            textPath: {
+                enabled: true,
+                attributes: {
+                    dy: 14,
+                    startOffset: '45%',
+                    textLength: 80
+                }
+            },
+            format: 'Node: {point.name}'
+        },
+        marker: {
+            radius: 35
+        },
+        data: [{
+            from: 'n1',
+            to: 'n2'
+        }, {
+            from: 'n2',
+            to: 'n3'
+        }, {
+            from: 'n3',
+            to: 'n4'
+        }, {
+            from: 'n4',
+            to: 'n5'
+        }, {
+            from: 'n5',
+            to: 'n1'
+        }]
+    }]
+});
